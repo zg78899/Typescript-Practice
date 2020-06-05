d2ba72123dbbf081a67130d2cc73bb1f53df8f50
kim jae hun <zg788990@gmail.com>
2020-06-05 17:19:46 +0900

    variable test var let const

--- variable/variables.ts
+++ variable/variables.ts
@@ -3,13 +3,20 @@ let score2=2;
 const defaultScore =0;
 function outer(){
   if(true){
-    var score=0;
+    let score:number;
+    // const score = 30 ;//반드시 선언과 할당을 해야한다.
+    score = 30;
+     
   }
-  for(var i =0;i<3;i++){
-    setTimeout(()=>{
+ for(let i =0;i<3;i++){
+   setTimeout(()=>{
       console.log(i);
-    },100)
-  }
+   },100)
+ }
   console.log(score);
 }
 outer();
+
+function outer(){
+
+}
