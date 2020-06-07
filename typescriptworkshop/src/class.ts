502d97f669719e57b7668983142aa7abd7a02faa
kim jae hun <zg788990@gmail.com>
2020-06-08 00:51:29 +0900

    class

---
+++ typescriptworkshop/src/class.ts
@@ -0,0 +1,48 @@
+interface User{
+  user:string;
+}
+interface Product{
+  id:string;
+  price:number;
+}
+
+class Cart{  //클래스(javascript)
+  //2개의 속성
+  
+  // protected user:User; // 기본저그오 아무런 접근제한자를 두지않으면 public이다. 
+  // private store:object; // 접근제한자 private,protected,public  private로 선언하면 class밖에서 접근할 수 없다. 
+//생성자 함수
+ constructor(public user : User,private store:object = {}){ //생성자에  매개변수에 접근제한자를 쓰게되면 속성이 정의됨과 동시에,
+                                                            //new 키워드로 호출할때 전달맏은 인자값이 Cart의 속성의 할당까지 된다.
+   //프로퍼티 
+  //  this.user = user;
+  //  this.store = {};
+ }
+ //2개의 메소드
+ public put(id:string,product:Product){
+  //  this.get(); // 접근제한자는 클래스 내부에서는  사용이 가능하다.
+  // this.user.name;
+   this.store[id] = product;
+ }
+ private get(id:string){
+   return this.store[id]
+ }
+}
+//다른 클래스에서 상속
+class PromotionCart extends Cart{
+  addPromotion(){
+    // this.user.name;
+  }
+}
+//인스턴스르 생성
+const cart2 = new PromotionCart({name:'john'});
+cart2.addPromotion();// Cart에서 상속한 put메소드
+// cart2.user
+const cartJohn = new Cart({name:'John'});
+const artJay = new Cart({name:'Jay'});
+
+//접근제한자 protected,private,public의 차이점 
+//1. 기본적으로 접근제한자를 설정해주지 않으면 public이다. public은 인스턴스에서라던지 클래스의 내부에서 접근이 가능하다.
+//2. privete와 protected는 인스턴스에서 점근이 불가능하다.그러나 클래스의 내부에서 접근이 가능하다.
+//3. private와 protected의 차이점은 private는 선언된 클래스의 내부에서만 사용이 가능하고 상속된 하위클래스를 비롯한 인스턴스에서 점근이 불가능하다.
+//4. protected는 선언된 클래스의 내부와 상속하고 있는 자신의 하위 클래스에서 상속이 가능하다. 
---
+++ typescriptworkshop/src/class2.ts
@@ -0,0 +1,49 @@
+//1.abstract 
+//2.implements 클래스에서 인터페이스를 구현하는 방법, 추상 클래스를 정의하는 방법
+//3. 추상 클래스를 인스턴스 시키기 위해서 새로운 타입을 만들어서 상속을 받고 . 거기에서 abstract 메소드들을 구현한다.
+
+interface Person{
+  name:string;
+  say(mesasge:string):void;
+}
+interface Programmer {
+  writeCode(requirement:string):string;
+}
+
+
+// 추상 클래스
+abstract class Korean implements Person{
+
+  public abstract jumin:number;
+
+  constructor(public name:string){
+
+  }
+  say(msg:string){
+    console.log(msg);
+  }
+  abstract loveKimchi():void;
+}
+
+
+class KoreanProgrammer extends Korean implements Programmer{
+
+  constructor(public name :string,public jumin:number){
+    super(name) //부모인 Korean클래스의 생성자함수를 호출해줘야한다. 
+  }
+  writeCode(requirement: string): string {
+    console.log(requirement);
+    return requirement + '...';
+  }
+
+  say(mesasge:string): void {
+    console.log(mesasge)
+  }
+  loveKimchi():void{
+    console.log()
+  }
+  
+}
+const jay = new KoreanProgrammer('jay',2222);
+// const jay2 = new Korean('jay'); //추상클래스의 인스턴스는 만들 수 없다. 꼭 상속을 받아서 하위타입에서 추상클래스를 구현을 해야한다. 
+//그리고 그 하위 타입을 우리가 인스턴스화 시킬 수 있는 것이다.(new 키워드를 사용하여 호출할수이다는 것을 의미) 
