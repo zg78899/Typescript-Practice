18105c6c62d05d7e252fce401bd5e924778e278c
kim jae hun <zg788990@gmail.com>
2020-07-29 15:08:16 +0900

    typescript baic

---
+++ ts-practice/dist/practice.js
@@ -0,0 +1,17 @@
+"use strict";
+let count = 0; //무조건 타입을 지정해 줄 필요는 없다.
+count += 1;
+// count = '문자열';
+const message = 'hello world';
+const done = false;
+//배열을 다룰때
+const number = [1, 2, 3, 4];
+const messages = ['hihi', 'hello'];
+//messages는 문자열로 이루어진 배열이기 때문에 
+// messages.push(1);
+//이 값은 문자열일수도 undefined일수도 있다.
+let mgitBeUndefined = undefined;
+let nullableNumber = null;
+let color = 'red';
+color = 'yellow';
+// color = 'green';//에러가 발생하게 됨
---
+++ ts-practice/src/practice.ts
@@ -0,0 +1,35 @@
+let count = 0; //무조건 타입을 지정해 줄 필요는 없다.
+count+=1;
+// count = '문자열';
+
+const message:string = 'hello world';
+const done :boolean = false;
+//배열을 다룰때
+const number :number[] = [1,2,3,4]
+;
+const messages : string[] = ['hihi','hello'];
+//messages는 문자열로 이루어진 배열이기 때문에 
+// messages.push(1);
+
+//이 값은 문자열일수도 undefined일수도 있다.
+let mgitBeUndefined:string | undefined = undefined;
+
+let nullableNumber:number | null = null;
+
+let color : 'red'|'yellow' ='red';
+color = 'yellow';
+// color = 'green';//에러가 발생하게 됨
+
+
+function sum(x:number,y:number):number{ //결과물도 숫자로 반환한다. 
+return x+y;
+};
+const result = sum(1,2);
+
+//배열의 안에 있는 모든 숫자 더하기
+function sumArray(number:number[]):number{
+  return number.reduce((acc,current)=>acc+current,0);
+}
+const total = sumArray([1,2,3,4,5]);
+console.log(total);
+
